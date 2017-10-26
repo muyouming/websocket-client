bb0a3fd7e9929c3b8679c93200fed75a8f1e450e
muyouming <muyouming@gmail.com>
2017-10-26 11:27:33 +0800

    Update _core.py
    change for LEMOC

--- websocket/_core.py
+++ websocket/_core.py
@@ -320,33 +320,37 @@ class WebSocket(object):
         return  value: tuple of operation code and string(byte array) value.
         """
         while True:
+#             frame = self.recv_frame()
+#             if not frame:
+#                 # handle error:
+#                 # 'NoneType' object has no attribute 'opcode'
+#                 raise WebSocketProtocolException(
+#                     "Not a valid frame %s" % frame)
+#             elif frame.opcode in (ABNF.OPCODE_TEXT, ABNF.OPCODE_BINARY, ABNF.OPCODE_CONT):
+#                 self.cont_frame.validate(frame)
+#                 self.cont_frame.add(frame)
+
+#                 if self.cont_frame.is_fire(frame):
+#                     return self.cont_frame.extract(frame)
+
+#             elif frame.opcode == ABNF.OPCODE_CLOSE:
+#                 self.send_close()
+#                 return frame.opcode, frame
+#             elif frame.opcode == ABNF.OPCODE_PING:
+#                 if len(frame.data) < 126:
+#                     self.pong(frame.data)
+#                 else:
+#                     raise WebSocketProtocolException(
+#                         "Ping message is too long")
+#                 if control_frame:
+#                     return frame.opcode, frame
+#             elif frame.opcode == ABNF.OPCODE_PONG:
+#                 if control_frame:
+#                     return frame.opcode, frame
+# change for LEMOC
             frame = self.recv_frame()
-            if not frame:
-                # handle error:
-                # 'NoneType' object has no attribute 'opcode'
-                raise WebSocketProtocolException(
-                    "Not a valid frame %s" % frame)
-            elif frame.opcode in (ABNF.OPCODE_TEXT, ABNF.OPCODE_BINARY, ABNF.OPCODE_CONT):
-                self.cont_frame.validate(frame)
-                self.cont_frame.add(frame)
-
-                if self.cont_frame.is_fire(frame):
-                    return self.cont_frame.extract(frame)
-
-            elif frame.opcode == ABNF.OPCODE_CLOSE:
-                self.send_close()
-                return frame.opcode, frame
-            elif frame.opcode == ABNF.OPCODE_PING:
-                if len(frame.data) < 126:
-                    self.pong(frame.data)
-                else:
-                    raise WebSocketProtocolException(
-                        "Ping message is too long")
-                if control_frame:
-                    return frame.opcode, frame
-            elif frame.opcode == ABNF.OPCODE_PONG:
-                if control_frame:
-                    return frame.opcode, frame
+            print("receive ok")
+            return ABNF.OPCODE_TEXT, frame
 
     def recv_frame(self):
         """
